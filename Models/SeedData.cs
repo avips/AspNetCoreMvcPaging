b3cd7e5795beac437c14771b79e8bfb2db179cac
avinp <avinp@ya.ru>
2022-01-16 16:17:00 +0300

    small changes

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -10,7 +10,8 @@ namespace ServerApp {
         public static void SeedDatabase(DataContext context) {
 
             context.Database.Migrate();
-
+            var processors = new[] { "AMD Ryzen 5 3500U", "AMD Ryzen 5 5600x", "Intel Core i5 10400", 
+                                     "Intel Core i5 10600", "AMD Ryzen 9 5900x" };
             if (context.Products.Count() == 0) {
                 for (var i = 1; i < 100; i++)
                 {
@@ -18,9 +19,9 @@ namespace ServerApp {
                     new Product
                     {
                         Name = $"Ноутбук {i}",
-                        Description = @"Экран 15.6 1920х1080; TN; Процессор AMD Ryzen 5 3500U 2.1ГГц;",
+                        Description = $"Экран 15.6 1920х1080; TN; Процессор {processors[i % 5]}",
                         Category = "Ноутбук",
-                        Price = 40000
+                        Price = 40000 + i * 100
                     });
                 }
                 context.SaveChanges();
